785445be319516b76f974bfdad44300e42eab32e
Jonathan <THEJAGSTER@Jagsters-Mac.local>
2020-09-21 07:51:39 -0600

    Added new column to Shows object

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -34,7 +34,8 @@ migrate = Migrate(app, db)
 # Added Shows object with Many-to-many relationship with Artists and Venues
 Shows = db.Table('Shows',
     db.Column('venue_id', db.Integer, db.ForeignKey('Venue.id'), primary_key=True),
-    db.Column('artist_id', db.Integer, db.ForeignKey('Artist.id'), primary_key=True)
+    db.Column('artist_id', db.Integer, db.ForeignKey('Artist.id'), primary_key=True),
+    db.Column('start_time', db.DateTime)
 )
 class Venue(db.Model):
     __tablename__ = 'Venue'
