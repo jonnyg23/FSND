939cc8334f6e97b948379640986390e31b1b5115
Jonathan <THEJAGSTER@Jagsters-Mac.local>
2020-09-25 04:18:23 -0600

    Completed TODO for artists()

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -517,16 +517,25 @@ def delete_venue(venue_id):
 @app.route('/artists')
 def artists():
   # TODO: replace with real data returned from querying the database
-  data=[{
-    "id": 4,
-    "name": "Guns N Petals",
-  }, {
-    "id": 5,
-    "name": "Matt Quevedo",
-  }, {
-    "id": 6,
-    "name": "The Wild Sax Band",
-  }]
+  # data=[{
+  #   "id": 4,
+  #   "name": "Guns N Petals",
+  # }, {
+  #   "id": 5,
+  #   "name": "Matt Quevedo",
+  # }, {
+  #   "id": 6,
+  #   "name": "The Wild Sax Band",
+  # }]
+
+  data = []
+  artists = Artist.query.order_by(Artist.name).all()
+  for artist in artists:
+    data.append({
+      "id": artist.id,
+      "name": artist.name
+    })
+    
   return render_template('pages/artists.html', artists=data)
 
 @app.route('/artists/search', methods=['POST'])
