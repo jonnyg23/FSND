ba764cfdde1e81a3659040536028e4c35592fef8
Jonathan <THEJAGSTER@Jagsters-Mac.local>
2020-10-07 00:36:28 -0600

    Setup CORS

--- projects/02_trivia_api/starter/backend/flaskr/__init__.py
+++ projects/02_trivia_api/starter/backend/flaskr/__init__.py
@@ -13,13 +13,14 @@ def create_app(test_config=None):
   app = Flask(__name__)
   setup_db(app)
   
-  '''
-  @TODO: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
-  '''
+  # Setup CORS
+  CORS(app)
 
   '''
   @TODO: Use the after_request decorator to set Access-Control-Allow
   '''
+  
+
 
   '''
   @TODO: 
