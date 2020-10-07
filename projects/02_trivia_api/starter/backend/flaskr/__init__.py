469b74f2870defee8e3f371c22b013dc8ff08160
Jonathan <THEJAGSTER@Jagsters-Mac.local>
2020-10-07 03:15:24 -0600

    Implemented delete_question() app route

--- projects/02_trivia_api/starter/backend/flaskr/__init__.py
+++ projects/02_trivia_api/starter/backend/flaskr/__init__.py
@@ -85,13 +85,28 @@ def create_app(test_config=None):
         'current_category': None
     })
 
-  '''
-  @TODO: 
+  ''' 
   Create an endpoint to DELETE question using a question ID. 
 
   TEST: When you click the trash icon next to a question, the question will be removed.
   This removal will persist in the database and when you refresh the page. 
   '''
+  @app.route('/questions/<question_id>', methods=['DELETE'])
+  def delete_question(question_id):
+    try:
+        # Query the question id from database
+        question = Question.query.get(question_id)
+        # Delete the question by referencing question id
+        question.delete()
+
+        return jsonify({
+            'success': True,
+            'deleted': question_id
+        })
+    except:
+        abort(422)
+
+
 
   '''
   @TODO: 
