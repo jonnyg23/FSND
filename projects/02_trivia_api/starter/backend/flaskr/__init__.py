64f9b61fe4b6d39496f959b30c223dcba54d2397
Jonathan <THEJAGSTER@Jagsters-Mac.local>
2020-10-07 03:41:21 -0600

    Added 405 error handler

--- projects/02_trivia_api/starter/backend/flaskr/__init__.py
+++ projects/02_trivia_api/starter/backend/flaskr/__init__.py
@@ -172,6 +172,14 @@ def create_app(test_config=None):
           'message': 'resource not found'
       }), 404
 
+  @app.errorhandler(405)
+  def method_not_allowed(error):
+      return jsonify({
+          'success': False,
+          'error': 405,
+          'message': 'method not allowed'
+      }), 405
+
   @app.errorhandler(422)
   def unprocessable(error):
       return jsonify({
@@ -179,6 +187,7 @@ def create_app(test_config=None):
           'error': 422,
           'message': 'unprocessable'
       }), 422
+
   
   return app
 
