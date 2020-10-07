5754240929bce3cb776445bbee97a90cd2b5ae0d
Jonathan <THEJAGSTER@Jagsters-Mac.local>
2020-10-07 01:16:28 -0600

    Fix app.errorhandler(422) indent

--- projects/02_trivia_api/starter/backend/flaskr/__init__.py
+++ projects/02_trivia_api/starter/backend/flaskr/__init__.py
@@ -141,13 +141,13 @@ def create_app(test_config=None):
           'message': 'resource not found'
       }), 404
 
-    @app.errorhandler(422)
-    def unprocessable(error):
-        return jsonify({
-            'success': False,
-            'error': 422,
-            'message': 'unprocessable'
-        }), 422
+  @app.errorhandler(422)
+  def unprocessable(error):
+      return jsonify({
+          'success': False,
+          'error': 422,
+          'message': 'unprocessable'
+      }), 422
   
   return app
 
