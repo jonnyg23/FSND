c1f3118630c955c5c4a3f3dfc1ff4c1b489cec1c
Jonathan <THEJAGSTER@Jagsters-Mac.local>
2020-10-07 04:00:07 -0600

    Added dtype to delete_question() app.route

--- projects/02_trivia_api/starter/backend/flaskr/__init__.py
+++ projects/02_trivia_api/starter/backend/flaskr/__init__.py
@@ -91,7 +91,7 @@ def create_app(test_config=None):
   TEST: When you click the trash icon next to a question, the question will be removed.
   This removal will persist in the database and when you refresh the page. 
   '''
-  @app.route('/questions/<question_id>', methods=['DELETE'])
+  @app.route('/questions/<int:question_id>', methods=['DELETE'])
   def delete_question(question_id):
     try:
         # Query the question id from database
