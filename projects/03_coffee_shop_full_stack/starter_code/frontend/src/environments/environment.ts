b4c0bf64aaa08a31bc9555782476d5d812952893
Jonathan <THEJAGSTER@Jagsters-Mac.local>
2020-10-21 14:11:20 -0600

    Update auth0 information

--- projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
+++ projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
@@ -4,11 +4,11 @@
 
 export const environment = {
   production: false,
-  apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
+  apiServerUrl: 'https://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: 'jg23-dev.us.auth0.com', // the auth0 domain prefix
+    url: 'jg23-dev.us', // the auth0 domain prefix
     audience: 'coffee', // the audience set for the auth0 app
     clientId: '7I4Y4XD0bywao22y8NjX2WZ6k0b2LA9M', // the client id generated for the auth0 app
-    callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
+    callbackURL: 'https://127.0.0.1:8100', // the base url of the running ionic application. 
   }
 };
